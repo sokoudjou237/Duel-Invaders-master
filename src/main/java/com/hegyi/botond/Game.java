23277349eff1c4f656e5b73da50f0b1906c9c0d1
larux1 <laruxtagne@gmail.com>
2022-12-05 17:37:25 +0100

    modif de game.java

--- src/main/java/com/hegyi/botond/Game.java
+++ src/main/java/com/hegyi/botond/Game.java
@@ -15,6 +15,10 @@ public class Game extends Canvas {
 	private Ship player;
 	private Fleet fleet;
 
+	private Ship player2;
+
+	private Fleet fleet2;
+
 	private GameObject background;
 
 	private myTimer timer = new myTimer();
@@ -22,6 +26,9 @@ public class Game extends Canvas {
 	private boolean inGame;
 
 	private int score = 0;
+
+	private int score2 = 0;
+
 
 	public static double WIDTH, HEIGHT;
 
@@ -41,6 +48,10 @@ public class Game extends Canvas {
 
 	public Ship getPlayer() {
 		return player;
+	}
+
+	public Ship getPlayer2() {
+		return player2;
 	}
 
 	public boolean isInGame() {
@@ -66,7 +77,11 @@ public class Game extends Canvas {
 
 		player = new Ship("images/shipSkin.png");
 
+		player2 = new Ship("images/shipSkin.png");
+
+
 		fleet = new Fleet("images/invader.png");
+		fleet2 = new Fleet("images/invader.png");
 
 		initBackground();
 
@@ -78,6 +93,10 @@ public class Game extends Canvas {
 	public void checkElements() {
 		player.check();
 		fleet.check();
+
+		player2.check();
+		fleet2.check();
+
 	}
 
 	private void updateElements(double elapsedTime) {
@@ -85,12 +104,19 @@ public class Game extends Canvas {
 			player.update(elapsedTime);
 			player.getBullet().update();
 			fleet.update(elapsedTime);
+
+			player2.update(elapsedTime);
+			player2.getBullet().update();
+			fleet2.update(elapsedTime);
 		}
 	}
 
 	private void renderScore() {
 		if (player.isAlive()) {
 			gc.fillText("Your score: " + Integer.toString(score), 3, 15);
+		}
+		if (player2.isAlive()) {
+			gc.fillText("Your score2: " + Integer.toString(score2), 3, 15);
 		}
 	}
 
@@ -100,23 +126,36 @@ public class Game extends Canvas {
 		if (fleet.intersect(player.getBullet(), gc)) {
 			score += 100;
 		}
+		if (fleet2.intersect(player2.getBullet(), gc)) {
+			score2 += 100;
+		}
 		fleet.intersect(player, gc);
 
+		fleet2.intersect(player2, gc);
+
+
 		fleet.render(gc);
+		fleet2.render(gc);
+
 
 		player.render(gc);
 		player.getBullet().render(gc);
 
+		player2.render(gc);
+		player2.getBullet().render(gc);
+
 		renderScore();
 	}
 
-	private void gameOver() {
+	private void gameOver(int scor) {
 		gc.setFill(Color.WHITE);
 		gc.setTextAlign(TextAlignment.CENTER);
 		gc.setTextBaseline(VPos.CENTER);
 		gc.setFont(new Font(gc.getFont().getName(), 30));
 
-		gc.fillText("Game over!\nYour score: " + score, WIDTH / 2.0, HEIGHT / 2.0);
+		gc.fillText("Game over!\nYour score: " + scor, WIDTH / 2.0, HEIGHT / 2.0);
+
+		gc.fillText("Game over!\nYour score: " + scor, WIDTH / 2.0, HEIGHT / 2.0);
 	}
 
 	public class myTimer extends AnimationTimer {
@@ -134,14 +173,27 @@ public class Game extends Canvas {
 			renderElements();
 
 			fleet.shoot();
+			fleet2.shoot();
 
 			if (!player.isAlive()) {
 				inGame = false;
-				gameOver();
+				gameOver(score);
+				this.stop();
+			}
+
+			if (!player2.isAlive()) {
+				inGame = false;
+				gameOver(score2);
 				this.stop();
 			}
 
 			if (fleet.isDestroyed()) {
+				System.out.println("You win");
+				inGame = false;
+				this.stop();
+			}
+
+			if (fleet2.isDestroyed()) {
 				System.out.println("You win");
 				inGame = false;
 				this.stop();
